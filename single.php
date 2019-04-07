b3f87f5fa083405f2a84f1f1171722f119f86967
Damion Lawrence <damionolawrence@gmail.com>
2019-04-07 17:47:16 +0200

    minor changes

--- single.php
+++ single.php
@@ -10,10 +10,8 @@ while(have_posts()) {
     </div>  
   </div>
     <div class="container container--narrow page-section">
-    <?php $backCategory = get_category_link(get_cat_ID( single_cat_title('',false) ) ); ?>
-
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo $backCategory; ?>"><i class="fa fa-home" aria-hidden="true"></i> Back Issues</a> <span class="metabox__main">Posted By <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+      <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>"><i class="fa fa-home" aria-hidden="true"></i> Back Issues</a> <span class="metabox__main">Posted By <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
 </div>
     
       <div class="generic-content">
